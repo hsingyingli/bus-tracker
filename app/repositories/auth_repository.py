4291bb8a2eb1d8547840acbf7a4152cf9239b812
hsingyingli <xingyingli855@gmail.com>
2025-04-12 14:50:18 +0800

    feat(AuthRepository): can get user by username

--- app/repositories/auth_repository.py
+++ app/repositories/auth_repository.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
+from typing import Optional
 
 from asyncpg.exceptions import UniqueViolationError
+
+from app.schemas.user import User
 
 
 class DuplicateUserError(Exception):
@@ -12,11 +15,21 @@ class AuthRepositoryInterface(ABC):
     async def register(self, username: str, email: str, hashed_password: str) -> None:
         raise NotImplementedError
 
+    @abstractmethod
+    async def get_user_by_username(self, username: str) -> Optional[User]:
+        raise NotImplementedError
+
 
 INSERT_USER_QUERY = """
     INSERT INTO users (username, email, password)
     VALUES ($1, $2, $3)
     RETURNING id
+"""
+
+GET_USER_BY_USERNAME_QUERY = """
+    SELECT id, username, email, password 
+    FROM users 
+    WHERE username = $1
 """
 
 
@@ -29,3 +42,9 @@ class AuthRepository(AuthRepositoryInterface):
             await self.conn.execute(INSERT_USER_QUERY, username, email, hashed_password)
         except UniqueViolationError:
             raise DuplicateUserError("username, email or password already exists")
+
+    async def get_user_by_username(self, username: str) -> Optional[User]:
+        row = await self.conn.fetchrow(GET_USER_BY_USERNAME_QUERY, username)
+        if row:
+            return User(**row)
+        return None
