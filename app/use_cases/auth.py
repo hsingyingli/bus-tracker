7b730417728f00fb2ea1e5ab01e68a8786627a2b
hsingyingli <xingyingli855@gmail.com>
2025-04-12 14:50:38 +0800

    feat(AuthUseCase): add login use case

--- app/use_cases/auth.py
+++ app/use_cases/auth.py
@@ -1,14 +1,18 @@
 from abc import ABC, abstractmethod
 
-from app.utils.hash import get_password_hash
-
 from app.repositories.auth_repository import (AuthRepositoryInterface,
                                               DuplicateUserError)
+from app.utils.hash import get_password_hash, verify_password
+from app.utils.token import create_access_token
 
 
 class AuthUseCaseInterface(ABC):
     @abstractmethod
     async def register(self, username: str, email: str, password: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def login(self, username: str, password: str):
         raise NotImplementedError
 
 
@@ -34,3 +38,15 @@ class AuthUseCase(AuthUseCaseInterface):
             return await self.auth_repository.register(username, email, hashed_password)
         except DuplicateUserError:
             raise RegisterUserError("User with this username already exists")
+
+    async def login(self, username: str, password: str) -> str:
+        user = await self.auth_repository.get_user_by_username(username)
+
+        if user is None:
+            raise UserNotFoundError("User not found")
+
+        if not verify_password(password, user.password):
+            raise UserPasswordMismatchError("Incorrect password")
+        access_token = create_access_token(user.username)
+
+        return access_token
